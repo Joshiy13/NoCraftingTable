bde4a12b9787d7a2aedbce40e14c74c6f47c6df0
Josh <joshua@weidanz.de>
2024-10-01 08:53:40 +0200

    Removed ability to use crafting table

---
+++ src/client/java/dev/joshiy13/nocraftingtable/client/NocraftingtableClient.java
@@ -0,0 +1,19 @@
+package dev.joshiy13.nocraftingtable.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.ActionResult;
+
+public class NocraftingtableClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+            if (world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.CRAFTING_TABLE) {
+                return ActionResult.FAIL; // Prevents the crafting table from being used
+            }
+            return ActionResult.PASS;
+        });
+    }
+}
---
+++ src/main/java/dev/joshiy13/nocraftingtable/Nocraftingtable.java
@@ -0,0 +1,10 @@
+package dev.joshiy13.nocraftingtable;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Nocraftingtable implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
